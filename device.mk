7c797e1ede6c2e4f79c80c7754e7ca2f2a947b10
ean-Pierre Rasquin <yank555.lu@gmail.com>
2016-10-01 10:02:52 +0200

    RMX1921: Don't compile SystemUITests
    Change-Id: Ib5114a12ecf2c6c3918a7d99867913946449a97e
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- device.mk
+++ device.mk
@@ -487,6 +487,9 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/nxp
 
+# SystemUI
+EXCLUDE_SYSTEMUI_TESTS := true
+
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \
