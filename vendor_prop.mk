0861a55b66d1665e92ab2078f77c66b901b95d1b
Simao Gomes Viana <xdevs23@outlook.com>
2018-02-11 18:21:48 +0100

    RMX1921: Tweak fling velocity system props
    Change-Id: I755a802e45fd07733e7efd42ad967f5a91190bb0
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -126,6 +126,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
 
+# Fling
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.min.fling_velocity=160 \
+    ro.max.fling_velocity=20000
+
 # FRP
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.frp.pst=/dev/block/bootdevice/by-name/frp
