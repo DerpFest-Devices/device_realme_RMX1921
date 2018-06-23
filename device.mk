3cbb39e9be8279a4da1e3ec89fc18640e5362045
Demon Singur <demonsingur@gmail.com>
2018-06-23 05:22:18 +0000

    Build separate system and vendor HIDL libs

--- device.mk
+++ device.mk
@@ -28,7 +28,9 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0_system
 
 # IMS
 PRODUCT_PACKAGES += \
