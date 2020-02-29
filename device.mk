77dd304e11e23849634e30349ea2b61d87553e6c
PIPIPIG233666 <pig.priv@gmail.com>
2020-02-29 13:13:18 -0500

    RMX1921: Exclude SystemUI overlay from RRO
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -250,7 +250,8 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay/packages/apps/Snap
+    $(LOCAL_PATH)/overlay/packages/apps/Snap \
+    $(LOCAL_PATH)/overlay/frameworks/base/packages/SystemUI
 
 # Permissions
 PRODUCT_COPY_FILES += \
