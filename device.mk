c0d1491a22e2ad124f3cd30a5a1271b167227ddf
dev-harsh1998 <dev-harsh1998@hotmail.com>
2020-03-08 15:09:27 +0530

    RMX1921: import XiaomiParts from sdm845-common as RealmeParts
    * revert included LineageOS/android_device_xiaomi_sdm845-common@6e445cf7126a6408bf4b8340310431418196c648
      we don't have pickup sensor.
    
    Signed-off-by: dev-harsh1998 <dev-harsh1998@hotmail.com>

--- device.mk
+++ device.mk
@@ -124,6 +124,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.2-service.clearkey
 
+# Doze
+PRODUCT_PACKAGES += \
+    RealmeParts
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.realme_sdm710 \
