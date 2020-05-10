6eea8ee7e8fc46d41f0b2f453a6ef5a5e2c21e94
pjgowtham <pjgowtham@gmail.com>
2020-05-10 23:22:15 +0530

    Include bluetooth permissions

--- device.mk
+++ device.mk
@@ -55,6 +55,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
+
 # Properties
 -include $(LOCAL_PATH)/vendor_prop.mk
 
