723c584d31e4c1fd7ff21082c992054df615553f
bgcngm <bgcngm@gmail.com>
2020-05-12 00:12:53 +0530

    sdm710-common :  Enable double tap to wake support

--- device.mk
+++ device.mk
@@ -137,7 +137,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
