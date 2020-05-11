634dbecb0ccfc1d8d9cc08ad5dd29dd6553b00bf
Anton Hansson <hansson@google.com>
2020-05-12 00:18:08 +0530

    Enforce RRO for all resource overlays

--- device.mk
+++ device.mk
@@ -140,6 +140,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage \
     $(LOCAL_PATH)/overlay
 
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
