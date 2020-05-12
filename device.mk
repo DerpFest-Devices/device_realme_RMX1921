eee813c1c883e59f36b436a78ce6064d9e69d734
pjgowtham <pjgowtham@gmail.com>
2020-05-12 23:06:48 +0530

    RMX1921 : Add gpio-keys.kl
    Event Hub : No device configuration file found for device gpio-keys

--- device.mk
+++ device.mk
@@ -151,6 +151,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ims-ext-common
 
+# Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.realme_sdm710
