5b10f0f0f405d14f2124fadb11c620bf45f33a72
pjgowtham <pjgowtham@gmail.com>
2020-05-13 19:23:18 +0530

    Add surfaceflinger props from stock rom
    These props are added in the prop file instead of the BoardConfig.mk because the configstore hal was deprecated.

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -179,6 +179,12 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
     persist.sys.sf.color_saturation=1.0 \
     persist.sys.sf.native_mode=0
     
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.has_wide_color_display=true \
+    ro.surface_flinger.has_HDR_display=true \
+    ro.surface_flinger.use_color_management=true \
+    ro.surface_flinger.wcg_composition_dataspace=143261696
+
 # Thermal
 PRODUCT_PROPERTY_OVERRIDES +=  \
     persist.sys.enable.oscar=0 \
