f0d93dfe2b5af09522e53605866396e4c7b90a6e
bgcngm <bgcngm@gmail.com>
2020-05-14 20:27:07 +0530

    Build thermal hal from source

--- device.mk
+++ device.mk
@@ -355,7 +355,8 @@ PRODUCT_PACKAGES += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@1.0-service \
+    thermal.sdm710
 
 # Trust HAL
 PRODUCT_PACKAGES += \
