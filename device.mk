3a75785c5a337ab429aff58e6b7bff144ea89aa2
codeworkx <daniel.hillenbrand@codeworkx.de>
2020-06-09 15:14:45 +0530

    Build vendor variants of  stdc++

--- device.mk
+++ device.mk
@@ -95,6 +95,7 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    libstdc++.vendor \
     Snap
 
 # Context Hub
