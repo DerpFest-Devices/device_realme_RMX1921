2a3cc97b73971bfa4fe4bfb1c08a43912bb96963
pjgowtham <pjgowtham@gmail.com>
2020-06-12 22:59:36 +0530

    sdm710-common : Add fingerprint changes

--- device.mk
+++ device.mk
@@ -126,7 +126,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.realme_sdm710 \
-    lineage.biometrics.fingerprint.inscreen@1.0-service.realme_sdm710
+    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.realme_sdm710 \
+    vendor.oppo.hardware.biometrics.fingerprint@2.1
 
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:vendor/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
