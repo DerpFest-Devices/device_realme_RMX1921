226d3165bc12298e13439cfd8ecee1ab9f0e4869
pjgowtham <pjgowtham@gmail.com>
2020-06-14 13:54:50 +0530

    sdm710-common : Build only the 64 bit variant of android.hardware.camera.provider

--- device.mk
+++ device.mk
@@ -95,7 +95,7 @@ TARGET_SCREEN_WIDTH := 1080
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.4-service_64 \
     libstdc++.vendor \
     Snap \
     vendor.qti.hardware.camera.device@1.0.vendor
