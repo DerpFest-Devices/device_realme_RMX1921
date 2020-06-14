b8fea755e718c1d7f5c787d3396c0ae1fb2888cc
bgcngm <bgcngm@gmail.com>
2020-06-14 08:54:00 +0530

    sdm710-common : Add PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE flag

--- device.mk
+++ device.mk
@@ -286,6 +286,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # Properties
+PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 -include $(LOCAL_PATH)/vendor_prop.mk
 
 # QTI
