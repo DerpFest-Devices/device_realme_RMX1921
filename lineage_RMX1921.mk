1585a98a693e1cd22acaf7f3b3660c3b7f34dd7c
pjgowtham <pjgowtham@gmail.com>
2020-06-19 19:02:26 +0530

    RMX1921 :Update build fingerprint along with minor cleanup

--- lineage_RMX1921.mk
+++ lineage_RMX1921.mk
@@ -7,16 +7,17 @@
 # Inherit from RMX1921 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-PRODUCT_BRAND := realme
+PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := RMX1921
-PRODUCT_MANUFACTURER := realme
+PRODUCT_MANUFACTURER := Realme
 PRODUCT_NAME := lineage_RMX1921
-PRODUCT_MODEL := realme XT
+PRODUCT_MODEL := Realme XT
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
-TARGET_VENDOR := realme
-TARGET_VENDOR_PRODUCT_NAME := RMX1921
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="sdm710-user 10 QKQ1.190918.001 release-keys"
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="unknown-user 10 QKQ1.190918.001 unknown release-keys" \
+    PRODUCT_NAME="RMX1921"
+    
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := sdm710-user-10-QKQ1.190918.001-release-keys
+BUILD_FINGERPRINT := realme/RMX1921/RMX1921:10/QKQ1.190918.001/1590390095:user/release-keys
