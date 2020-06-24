d01d2cf17ea917a60b9ff3651d2c216d1bbacde0
sukrut4778 <sukrutbhagwat4778@gmail.com>
2020-06-24 20:41:50 +0530

    Dont build hwcomposer from source
    Since it breaks fod dimlayer

--- device.mk
+++ device.mk
@@ -115,7 +115,6 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     gralloc.sdm710 \
-    hwcomposer.sdm710 \
     memtrack.sdm710 \
     libtinyxml \
     libvulkan \
