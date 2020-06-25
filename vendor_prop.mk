8e5d196c0b2ff1d31cdc0b6488e0cf1559389940
pjgowtham <pjgowtham@gmail.com>
2020-06-25 18:31:50 +0530

    Use stock codecs props
    this seems to fix the freeze that occurs when using google chrome

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -98,7 +98,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
    
 # Codec2
 PRODUCT_PROPERTY_OVERRIDES +=  \
-    debug.media.codec2=2
+   debug.stagefright.omx_default_rank.sw-audio=1 \
+   debug.stagefright.omx_default_rank=0
  
 # Display post-processing
 PRODUCT_PROPERTY_OVERRIDES +=  \
