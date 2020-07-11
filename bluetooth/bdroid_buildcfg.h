d5d3d8f4e9a27566ecb429b33e5f71ed1db0d7e9
pjgowtham <pjgowtham@gmail.com>
2020-07-11 22:53:40 +0530

    RMX1921 : Set bluetooth device name

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -21,7 +21,7 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-#define BTM_DEF_LOCAL_NAME   "QCOM-BTD"
+#define BTM_DEF_LOCAL_NAME   "Realme XT"
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
