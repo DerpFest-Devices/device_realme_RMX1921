0dbd32ecd183df55dbe73e99f602d240cbd9e355
Harshit Jain <god@harsh1998.me>
2020-07-16 04:51:42 +0530

    RMX1921: init: add permissions for devicesettings nodes

--- device.mk
+++ device.mk
@@ -320,6 +320,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.class_main.sh \
+    init.devicesetting.rc \
     init.oppo.reserve.rc \
     init.msm.usb.configfs.rc \
     init.oppo.fingerprints.sh \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -112,3 +112,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.devicesetting.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.devicesetting.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+include $(BUILD_PREBUILT)
