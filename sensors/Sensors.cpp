b8acad91032cd8b5b745a4e7e0aefd57b968b046
kaderbava <ksbava7325@gmail.com>
2020-07-30 14:43:47 +0000

    Revert "RMX1921: sensors: partially revert ba4370e8ab0e1dc4337bdc3a8c2fa818edde7c94"
    This reverts commit ee5a5608e846868136f2bbbe29282e7468ff6232.

--- sensors/Sensors.cpp
+++ sensors/Sensors.cpp
@@ -129,10 +129,15 @@ Return<void> Sensors::getSensorsList(getSensorsList_cb _hidl_cb) {
 
         convertFromSensor(*src, dst);
 
-	    if (dst->typeAsString == "qti.sensor.proximity_fake") {
-            dst->type = SensorType::PROXIMITY;
-            mSensorHandleProximity = dst->sensorHandle;
+        if (dst->typeAsString == "qti.sensor.wise_light") {
+            dst->type = SensorType::LIGHT;
             dst->typeAsString = "";
+        }
+
+	 if (dst->typeAsString == "qti.sensor.proximity_fake") {
+             dst->type = SensorType::PROXIMITY;
+             mSensorHandleProximity = dst->sensorHandle;
+             dst->typeAsString = "";
         }
 
         // For proximity Wakeup
