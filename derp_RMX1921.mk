59ef80cbb21a1ef873746bef02e1563b0af32a28
kaderbava <ksbava7325@gmail.com>
2020-08-06 11:54:53 +0000

    RMX1921: Update Coral August Fingerprint

--- derp_RMX1921.mk
+++ derp_RMX1921.mk
@@ -26,8 +26,8 @@ PRODUCT_MODEL := Realme XT
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200705.002 6506677 release-keys" \
+    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200805.001 6578210 release-keys" \
     PRODUCT_NAME="RMX1921"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys
