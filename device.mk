f265229c7148ba0eb6c7d0f250495410ec492a52
Adhitya Mohan <me@adhityamohan.in>
2020-09-13 19:28:54 +0530

    RMX1921: Add hardware/google/pixel to namespace
    - Else it won't gib lib RART

--- device.mk
+++ device.mk
@@ -396,7 +396,8 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/qcom-caf/sdm845
+    hardware/qcom-caf/sdm845 \
+    hardware/google/pixel
 
 # Telephony
 PRODUCT_PACKAGES += \
