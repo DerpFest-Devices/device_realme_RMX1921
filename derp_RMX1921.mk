89023fec77de73528c9e26da6605335373d1f121
kaderbava <ksbava7325@gmail.com>
2020-09-22 20:03:25 +0000

    RMX1921: override build fingerprint
    * Required for passsing safetynet in R
    
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- derp_RMX1921.mk
+++ derp_RMX1921.mk
@@ -31,3 +31,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
