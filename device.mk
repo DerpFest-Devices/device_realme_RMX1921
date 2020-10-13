ThatisJigen/android_device_realme_RMX1921

RMX1921: Add input group to sensors service
* Replace uhid with input
* service as is from hardware/interfaces
* Binderize sensor hal

device.mk
@@ -375,7 +375,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl.xt \
-    android.hardware.sensors@1.0-service \
+    android.hardware.sensors@1.0-service.xt \
     libsensorndkbridge
 
 # Soong namespaces

DerpFest-Devices/device_realme_RMX1921

device.mk
@@ -363,7 +363,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl.realme_sdm710 \
-    android.hardware.sensors@1.0-service \
+    android.hardware.sensors@1.0-service.realme_sdm710 \
     libsensorndkbridge
 
 # Soong namespaces