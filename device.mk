b262ec9a40f913b21f37b0070b3bcf5df35b25fd
Danny Lin <danny@kdrag0n.dev>
2020-11-03 13:04:47 -0800

    RMX1921: Build QuickAccessWallet plugin for SystemUI
    This plugin that provides the wallet (cards and passes) integration in
    the power menu is now open-source, so let's build it.
    
    Change-Id: I5df8a09c8f91805df538bc7ddad6d326f5d9b13e
    Signed-off-by: improbir <ghosh_probir@yahoo.com>
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -444,6 +444,10 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
+# Wallet
+PRODUCT_PACKAGES += \
+    QuickAccessWallet
+
 # WiFi Display
 PRODUCT_PACKAGES += \
     libdisplayconfig \
