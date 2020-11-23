9c4e93ec81879ac5daf8430fdb9771f273e04dcb
Michael Bestas <mkbestas@lineageos.org>
2020-11-23 20:47:24 +0200

    RMX1921: Don't build textclassifier
    * Target no longer exists
    
    Change-Id: I2079d7dbbeb59bbe9cd925a56f6bcc84c8a38d7f
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -411,10 +411,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# TextClassifier
-PRODUCT_PACKAGES += \
-    textclassifier.bundle1
-
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.RMX1921
@@ -423,7 +419,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
 
-#Thermal
+# Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
