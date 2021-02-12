e5c0e6f694e7d722de988346fe60fdd89459b853
lucky <ah440099@gmail.com>
2021-02-12 15:07:02 +0200

    RMX1921: Remove more legacy ril-daemon rules
    * W libc : Unable to set property "ctl.stop" to "vendor.ril-daemon": error code: 0x20

--- rootdir/etc/init.class_main.sh
+++ rootdir/etc/init.class_main.sh
@@ -38,7 +38,6 @@ qcrild_status=true
 case "$baseband" in
     "apq" | "sda" | "qcs" )
     setprop ro.vendor.radio.noril yes
-    stop vendor.ril-daemon
     stop vendor.qcrild
 esac
 
@@ -85,8 +84,6 @@ case "$baseband" in
     if [ "$qcrild_status" = "true" ]; then
         # Make sure both rild, qcrild are not running at same time.
         # This is possible with vanilla aosp system image.
-        stop vendor.ril-daemon
-
         start vendor.qcrild
     else
         start vendor.ril-daemon
