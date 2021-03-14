133a1168eef298ced97992c310d80f1738f968e2
dianlujitao <dianlujitao@gmail.com>
2021-03-14 19:18:41 +0000

    RMX1921: Switch to source built vendor.qti.hardware.perf@2.0.so

--- device.mk
+++ device.mk
@@ -318,7 +318,8 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power.stats@1.0-service.mock \
+    vendor.qti.hardware.perf@2.0.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
