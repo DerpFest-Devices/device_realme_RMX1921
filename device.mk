7d8efb74a87c61fa07d80c17a0111af458a7da2a
Chenyang Zhong <zhongcy95@gmail.com>
2021-04-15 05:34:29 -0400

    RMX1921: move dummy metadata to init
    system/sepolicy/private/apexd.te has neverallows that prevent
    vendor_init from creating directories labeled as apex_metadata_file.
    Move the code to /system/etc/init/apex_metadata.rc so that scontext
    becomes init instead of vendor_init.
    
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -386,7 +386,8 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.sh \
     init.recovery.qcom.rc \
     init.target.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    apex_metadata.rc
 
 # Recovery
 PRODUCT_PACKAGES += \
