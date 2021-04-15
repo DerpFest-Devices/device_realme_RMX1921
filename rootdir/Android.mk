17dc589d2c5b2ecaa7099b407ff0fc843bc37247
Chenyang Zhong <zhongcy95@gmail.com>
2021-04-15 05:34:29 -0400

    RMX1921: move dummy metadata to init
    system/sepolicy/private/apexd.te has neverallows that prevent
    vendor_init from creating directories labeled as apex_metadata_file.
    Move the code to /system/etc/init/apex_metadata.rc so that scontext
    becomes init instead of vendor_init.
    
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -120,3 +120,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.devicesetting.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := apex_metadata.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/apex_metadata.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT)/etc/init
+include $(BUILD_PREBUILT)
