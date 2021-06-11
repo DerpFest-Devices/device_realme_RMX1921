80c3b6c79fe8803ff87cf778fd30b649efdac1c9
kaderbava <ksbava7325@gmail.com>
2021-06-11 21:44:02 +0000

    RMX1921: Partially revert a6db9c8
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -333,8 +333,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service \
+    android.hardware.power-service-qti \
     android.hardware.power.stats@1.0-service.mock \
     vendor.qti.hardware.perf@2.0.vendor
 
