0d9ef6f2095f8eee99918623a95a845f2d45e920
trpsl <trpsl755@gmail.com>
2021-06-17 11:54:15 +0000

    RMX1921: Update Moto Dolby from Moto-G Pro
    * From sofiap_ao_eea-user-11-RPR31.Q4U-20-26-2-a10bb-release-keys
    * Values taken from OP9 Pro and modified by Sasha (@helenius147 on Telegram) to work with Moto Dolby.
    
    Co-authored-by: blankaf <realblankaf@gmail.com>
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService CarrierSetup ConnMO DCMO DMService MyVerizonServices RecorderPrebuilt Maps YouTube Photos Drive PrebuiltGmail arcore SoundAmplifierPrebuilt SafetyHubPrebuilt OBDM_Permissions obdm_stub Showcase SprintDM SprintHM USCCDM VZWAPNLib VzwOmaTrigger
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService CarrierSetup ConnMO DCMO DMService MyVerizonServices RecorderPrebuilt Maps YouTube Photos Drive PrebuiltGmail arcore SoundAmplifierPrebuilt SafetyHubPrebuilt OBDM_Permissions obdm_stub Showcase SprintDM SprintHM USCCDM VZWAPNLib VzwOmaTrigger MusicFX
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
