d2eb9f3af64710da93fac94821b8bdab6141cd5f
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-08-03 21:18:02 +0000

    RMX1921: libinit: handle dalvik props based on sysram
    * Adapted to Realme XT.
    * We have 3 variants(4gb, 6gb & 8gb RAM)
    * Values are taken from frameworks/native
    
    Co-authored-by: kaderbava <ksbava7325@gmail.com>
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- init/init_RMX1921.cpp
+++ init/init_RMX1921.cpp
@@ -13,6 +13,7 @@
 
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
+#include <sys/sysinfo.h>
 
 #include <android-base/properties.h>
 #include "property_service.h"
@@ -42,6 +43,34 @@ void property_override(char const prop[], char const value[], bool add = true) {
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
+void load_dalvikvm_properties() {
+  struct sysinfo sys;
+  sysinfo(&sys);
+  if (sys.totalram > 6144ull * 1024 * 1024) {
+    // from - phone-xhdpi-8192-dalvik-heap.mk
+    property_override("dalvik.vm.heapstartsize", "24m");
+    property_override("dalvik.vm.heapgrowthlimit", "256m");
+    property_override("dalvik.vm.heaptargetutilization", "0.46");
+    property_override("dalvik.vm.heapmaxfree", "48m");
+    }
+  else if (sys.totalram > 4096ull * 1024 * 1024) {
+    // from - phone-xhdpi-6144-dalvik-heap.mk
+    property_override("dalvik.vm.heapstartsize", "16m");
+    property_override("dalvik.vm.heapgrowthlimit", "256m");
+    property_override("dalvik.vm.heaptargetutilization", "0.5");
+    property_override("dalvik.vm.heapmaxfree", "32m");
+    }
+  else {
+    // from - phone-xhdpi-4096-dalvik-heap.mk
+    property_override("dalvik.vm.heapstartsize", "8m");
+    property_override("dalvik.vm.heapgrowthlimit", "192m");
+    property_override("dalvik.vm.heaptargetutilization", "0.6");
+    property_override("dalvik.vm.heapmaxfree", "16m");
+  }
+  property_override("dalvik.vm.heapsize", "512m");
+  property_override("dalvik.vm.heapminfree", "8m");
+}
+
 void vendor_load_properties() {
 
     string oppo_sku = GetProperty("ro.boot.product.hardware.sku", "");
@@ -53,4 +82,7 @@ void vendor_load_properties() {
     } else if (oppo_sku == "secure_element_uicc") {
         property_override("ro.boot.product.hardware.sku", "RMX1921");
     }
+
+  // dalvikvm props
+  load_dalvikvm_properties();
 }
--- vendor_prop.mk
+++ vendor_prop.mk
@@ -109,15 +109,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
    debug.stagefright.omx_default_rank.sw-audio=1 \
    debug.stagefright.omx_default_rank=0
 
-# Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-   dalvik.vm.heapstartsize=16m \
-   dalvik.vm.heapgrowthlimit=384m \
-   dalvik.vm.heapsize=512m \
-   dalvik.vm.heaptargetutilization=0.75 \
-   dalvik.vm.heapminfree=4m \
-   dalvik.vm.heapmaxfree=16m
-
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.enable_default_color_mode=1 \
