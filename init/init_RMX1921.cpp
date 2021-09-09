bd5ddd42e7ad1a703903fede6f602864eb03ce2b
kaderbava <ksbava7325@gmail.com>
2021-09-09 11:14:19 +0000

    RMX1921: Tune dalvik heap props for 4gb ram variants
    * It will fix ui lags on 4gb ram variants.
    
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- init/init_RMX1921.cpp
+++ init/init_RMX1921.cpp
@@ -49,24 +49,22 @@ void load_dalvikvm_properties() {
   if (sys.totalram > 6144ull * 1024 * 1024) {
     // from - phone-xhdpi-8192-dalvik-heap.mk
     property_override("dalvik.vm.heapstartsize", "24m");
-    property_override("dalvik.vm.heapgrowthlimit", "256m");
     property_override("dalvik.vm.heaptargetutilization", "0.46");
     property_override("dalvik.vm.heapmaxfree", "48m");
     }
   else if (sys.totalram > 4096ull * 1024 * 1024) {
     // from - phone-xhdpi-6144-dalvik-heap.mk
     property_override("dalvik.vm.heapstartsize", "16m");
-    property_override("dalvik.vm.heapgrowthlimit", "256m");
     property_override("dalvik.vm.heaptargetutilization", "0.5");
     property_override("dalvik.vm.heapmaxfree", "32m");
     }
   else {
     // from - phone-xhdpi-4096-dalvik-heap.mk
     property_override("dalvik.vm.heapstartsize", "8m");
-    property_override("dalvik.vm.heapgrowthlimit", "192m");
-    property_override("dalvik.vm.heaptargetutilization", "0.6");
+    property_override("dalvik.vm.heaptargetutilization", "0.7");
     property_override("dalvik.vm.heapmaxfree", "16m");
   }
+  property_override("dalvik.vm.heapgrowthlimit", "256m");
   property_override("dalvik.vm.heapsize", "512m");
   property_override("dalvik.vm.heapminfree", "8m");
 }
