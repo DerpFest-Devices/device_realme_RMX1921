964dcea08b764773a375a60f8a1ac9e2c4b7b9be
kaderbava <ksbava7325@gmail.com>
2021-10-12 08:45:12 +0000

    RMX1921: AEX-ify
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_RMX1921.mk
+    $(LOCAL_DIR)/aosp_RMX1921.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_RMX1921-userdebug \
-    evolution_RMX1921-eng
+    aosp_RMX1921-userdebug \
+    aosp_RMX1921-eng
--- aosp_RMX1921.mk
+++ aosp_RMX1921.mk
@@ -4,13 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Evolution-X stuff
-TARGET_GAPPS_ARCH := arm64
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_BOOT_ANIMATION_RES := 1080
-WITH_GAPPS := true
-EXTRA_FOD_ANIMATIONS := true
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common AEX stuff
+TARGET_BOOT_ANIMATION_RES := 2280
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Inherit from RMX1921 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -18,7 +14,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := RMX1921
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := evolution_RMX1921
+PRODUCT_NAME := aosp_RMX1921
 PRODUCT_MODEL := Realme XT
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
