94d9a94228302520304b5bd222ac92791ae1f576
ThatisJigen <gianfranco.liguori.96@gmail.com>
2021-11-03 20:41:11 +0100

    RMX1921: declare soong namespace and build NFC package

--- device.mk
+++ device.mk
@@ -281,7 +281,8 @@ PRODUCT_PACKAGES += \
     NfcNci \
     nqnfcee_access.xml \
     nqnfcse_access.xml \
-    Tag
+    Tag \
+    nfc_nci_nxp
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
@@ -424,7 +425,8 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     kernel/realme/RMX1921 \
     hardware/qcom-caf/sdm845 \
-    hardware/google/pixel
+    hardware/google/pixel \
+    hardware/nxp
 
 # Telephony
 PRODUCT_PACKAGES += \
