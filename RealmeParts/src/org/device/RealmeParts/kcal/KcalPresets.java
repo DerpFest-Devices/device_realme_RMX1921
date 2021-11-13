d19f7819b52d9502c7aae265db8b677dade510b0
ThatisJigen <gianfranco.liguori.96@gmail.com>
2021-11-13 17:09:37 +0100

    RMX1921: RealmeParts: add some more KCal presets *took from XDA comments *also fixup min and max Kelvin values

--- RealmeParts/src/org/device/RealmeParts/kcal/KcalPresets.java
+++ RealmeParts/src/org/device/RealmeParts/kcal/KcalPresets.java
@@ -20,16 +20,16 @@ package org.device.RealmeParts.kcal;
 
 public class KcalPresets {
 
-    public static final String[] red = {"256", "225", "250", "240", "256", "250", "250", "236", "256", "253", "226"};
-    public static final String[] green = {"256", "245", "250", "240", "250", "250", "250", "248", "256", "246", "215"};
-    public static final String[] blue = {"256", "256", "235", "240", "251", "256", "256", "256", "256", "243", "256"};
-    public static final String[] satIntensity = {"255", "264", "251", "257", "290", "284", "257", "274", "289", "274", "264"};
-    public static final String[] scrContrast = {"255", "255", "260", "255", "260", "264", "264", "258", "264", "258", "260"};
-    public static final String[] scrHue = {"0", "0", "1520", "0", "1526", "0", "0", "0", "0", "0", "10"};
-    public static final String[] scrValue = {"255", "255", "240", "255", "264", "245", "245", "251", "242", "251", "247"};
+    public static final String[] red = {"256", "225", "250", "240", "256", "250", "250", "236", "256", "253", "226", "225", "230", "237", "245", "240", "230", "217", "237", "230"};
+    public static final String[] green = {"256", "245", "250", "240", "250", "250", "250", "248", "256", "246", "215", "225", "232", "235", "245", "256", "240", "215", "239", "232"};
+    public static final String[] blue = {"256", "256", "235", "240", "251", "256", "256", "256", "256", "243", "256", "237", "255", "255", "256", "256", "255", "255", "256", "255"};
+    public static final String[] satIntensity = {"255", "264", "251", "257", "290", "284", "257", "274", "289", "274", "264", "277", "277", "265", "273", "290", "285", "265", "270", "274"};
+    public static final String[] scrContrast = {"255", "255", "260", "255", "260", "264", "264", "258", "264", "258", "260", "264", "243", "255", "255", "264", "270", "250", "275", "268"};
+    public static final String[] scrHue = {"0", "0", "1520", "0", "1526", "0", "0", "0", "0", "0", "10", "0", "0", "1515", "10", "0", "0", "0", "0", "0"};
+    public static final String[] scrValue = {"255", "255", "240", "255", "264", "245", "245", "251", "242", "251", "247", "250", "273", "253", "265", "242", "240", "253", "247", "247"};
 
     enum Presets {
-        DEFAULT, VERSION1, VERSION2, VERSION3, TRILUMINOUS, DEEPBW, DEEPND, COOLAMOLED, EXTREMEAMOLED, WARMAMOLED, HYBRIDMAMBA;
+        DEFAULT, VERSION1, VERSION2, VERSION3, TRILUMINOUS, DEEPBW, DEEPND, COOLAMOLED, EXTREMEAMOLED, WARMAMOLED, HYBRIDMAMBA, DEEPAMOLED, COLDBRIGHTAMOLED, CLEANDEEP, SHAMSHUNG, NATURE, SHAMSHUNGCOLD, COLDASICE, DEEPSAT, BLACKAF;
         public static Presets toEnum(int index) {
             switch (index) {
                 case 0:
@@ -54,6 +54,24 @@ public class KcalPresets {
                     return WARMAMOLED;
                 case 10:
                     return HYBRIDMAMBA;
+                case 11:
+                    return DEEPAMOLED;
+                case 12:
+                    return COLDBRIGHTAMOLED;
+                case 13:
+                    return CLEANDEEP;
+                case 14:
+                    return SHAMSHUNG;
+                case 15:
+                    return NATURE;
+                case 16:
+                    return SHAMSHUNGCOLD;
+                case 17:
+                    return COLDASICE;
+                case 18:
+                    return DEEPSAT;
+                case 19:
+                    return BLACKAF;
             }
             return null;
         }
