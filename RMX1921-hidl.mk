7e13e992a7c515ee9a3cb4f4665fdd5772279aed
ThatisJigen <gianfranco.liguori.96@gmail.com>
2021-12-14 22:24:47 +0100

    RMX1921: build and ship android.hardware.drm@1.4

--- RMX1921-hidl.mk
+++ RMX1921-hidl.mk
@@ -56,7 +56,9 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.2 \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3 \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.3.vendor \
+    android.hardware.drm@1.4 \
+    android.hardware.drm@1.4.vendor
 
 # GPS
 PRODUCT_PACKAGES += \
