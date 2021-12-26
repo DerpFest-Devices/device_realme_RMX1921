90e95ef784b66f5ba5c5b45f824eb137808a056e
kaderbava <ksbava7325@gmail.com>
2021-12-26 12:36:01 +0000

    RMX1921: Build udfps animations
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- derp_RMX1921.mk
+++ derp_RMX1921.mk
@@ -6,6 +6,7 @@
 
 # Inherit some common DerpFest stuff
 TARGET_BOOT_ANIMATION_RES := 1080
+EXTRA_UDFPS_ANIMATIONS := true
 $(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Official-ify
