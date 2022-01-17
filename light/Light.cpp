3149647c6438f4bb10f026b408335bf50e6539c1
vasishath <vasishath@gmail.com>
2022-01-17 14:10:40 +0100

    RMX1921: light: Detect maximum brightness dynamically
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- light/Light.cpp
+++ light/Light.cpp
@@ -19,8 +19,27 @@
 #include <android-base/file.h>
 #include <android-base/logging.h>
 #include <fcntl.h>
+#include <fstream>
 
 using ::android::base::WriteStringToFile;
+
+namespace {
+/*
+ * Read max brightness from path and close file.
+ */
+static int getMaxBrightness(std::string path) {
+    std::ifstream file(path);
+    int value;
+
+    if (!file.is_open()) {
+        LOG(WARNING) << "failed to read from " << path.c_str();
+        return 0;
+    }
+
+    file >> value;
+    return value;
+}
+} // anonymous namespace
 
 namespace aidl {
 namespace android {
@@ -30,7 +49,7 @@ namespace light {
 static const std::string kLCDFile = "/sys/class/backlight/panel0-backlight/brightness";
 
 #define AutoHwLight(light) {.id = (int)light, .type = light, .ordinal = 0}
-#define MAX_LCD_BRIGHTNESS    1023
+#define MAX_BRIGHTNESS_PATH    "/sys/class/backlight/panel0-backlight/max_brightness"
 
 // List of supported lights
 const static std::vector<HwLight> kAvailableLights = {
@@ -63,7 +82,7 @@ uint32_t Lights::RgbaToBrightness(uint32_t color) {
     uint32_t red = (color >> 16) & 0xFF;
     uint32_t green = (color >> 8) & 0xFF;
     uint32_t blue = color & 0xFF;
-
+    int max_brightness = getMaxBrightness(MAX_BRIGHTNESS_PATH);
     /*
      * Scale RGB brightness if Alpha brightness is not 0xFF.
      */
@@ -74,10 +93,10 @@ uint32_t Lights::RgbaToBrightness(uint32_t color) {
     }
     brightness=((77 * red + 150 * green + 29 * blue) >> 8);
     // Scale the value for our panel
-    brightness=((brightness - 1) * (MAX_LCD_BRIGHTNESS - 1) / (0xFF - 1) + 1);
+    brightness=((brightness - 1) * (max_brightness - 1) / (0xFF - 1) + 1);
     // Prevent errors
-    if (brightness>=1024)
-        brightness=1023;
+    if (brightness > max_brightness)
+        brightness = max_brightness;
     return brightness;
 }
 
