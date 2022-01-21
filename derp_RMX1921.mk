864f96341e0a8f5921e08e8353032b1ee7863b93
kaderbava <ksbava7325@gmail.com>
2022-01-21 09:41:24 +0000

    RMX1921: Use stock fingerprint
    * From RUI 2.0 F03
    * SafetyNet props are handling by rom sources now. So we can use stock fp.
    
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- derp_RMX1921.mk
+++ derp_RMX1921.mk
@@ -24,11 +24,8 @@ PRODUCT_MODEL := Realme XT
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.220105.002 7961164 release-keys" \
+    PRIVATE_BUILD_DESC="RMX1921-user 11 RKQ1.201217.002 1626947099367 release-keys" \
     PRODUCT_NAME="RMX1921"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+BUILD_FINGERPRINT := realme/RMX1921/RMX1921:11/RKQ1.201217.002/1626947099367:user/release-keys
