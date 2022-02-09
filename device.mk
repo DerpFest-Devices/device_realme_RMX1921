66e766cf8ea5571df102348542e83b4fd8e79adb
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-02-09 15:31:47 +0100

    RMX1921: Define Bluetooth capabilities through component overrides
    Change-Id: I6cf902e0ca554aa4178a1eb6de7824a10e6e0645
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -120,6 +120,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libsuspend
 
+# Component overrides
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+
 # Context Hub
 PRODUCT_PACKAGES += \
     android.hardware.contexthub@1.0-impl.generic \
