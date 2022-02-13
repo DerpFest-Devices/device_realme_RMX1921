cc26565b9f1d8b9b79f64cac8c14605e49278385
adi8900 <adrianszymanski242@gmail.com>
2022-02-13 23:58:48 +0100

    RMX1921: Ship missing qti_whitelist_system_ext.xml
    * From LA.QSSI.12.0.r1-05800.02-qssi.0
    
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -358,6 +358,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
+    $(LOCAL_PATH)/configs/qti_whitelist_system_ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/qti_whitelist_system_ext.xml \
     $(LOCAL_PATH)/configs/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
 
 # Radio
