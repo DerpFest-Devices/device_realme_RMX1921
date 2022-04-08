9a8d93c61576b77f48f0919526eb5c2aef69fa92
kaderbava <ksbava7325@gmail.com>
2022-04-08 12:26:57 +0000

    Revert "RMX1921: Udfps: Add show|hide udfps view callbacks to IBiometricsFingerprint@2.3"
    This reverts commit 81bdb4826569f99ec99ed2955fe8f66d74792357.

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -307,14 +307,6 @@ Return<void> BiometricsFingerprint::onFingerUp() {
     return Void();
 }
 
-Return<void> BiometricsFingerprint::onHideUdfpsOverlay() {
-    return Void();
-}
-
-Return<void> BiometricsFingerprint::onShowUdfpsOverlay() {
-    return Void();
-}
-
 }  // namespace implementation
 }  // namespace V2_3
 }  // namespace fingerprint
--- fingerprint/BiometricsFingerprint.h
+++ fingerprint/BiometricsFingerprint.h
@@ -66,8 +66,6 @@ public:
     Return<bool> isUdfps(uint32_t sensorId) override;
     Return<void> onFingerDown(uint32_t x, uint32_t y, float minor, float major) override;
     Return<void> onFingerUp() override;
-    Return<void> onShowUdfpsOverlay() override;
-    Return<void> onHideUdfpsOverlay() override;
 
 private:
     sp<vendor::oppo::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprint> mOppoBiometricsFingerprint;
