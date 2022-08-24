4eefd6c0f39f6651cecbe7a61638e5d458e3897d
Tim Zimmermann <tim@linux4.de>
2022-08-24 15:40:22 +0200

    RMX1921: Inherit non_ab_device.mk
    * This is required to build applypatch for updating recovery
    
    Change-Id: I38053691f7724b5d6c30159c410d5d04ac136180
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -10,6 +10,7 @@ $(call inherit-product, build/make/target/product/product_launched_with_p.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/realme/RMX1921/RMX1921-vendor.mk)
