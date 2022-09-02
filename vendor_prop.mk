44a89c43954a8a16f1d065aa274cf37eff71404a
LuK1337 <priv.luk@gmail.com>
2022-09-02 14:14:28 +0200

    RMX1921: Pull appropriate USB props from vendor_product.mk
    Change-Id: I93b25c5d0989c83d1c8d7b7946a083771febdddd
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -195,9 +195,15 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # USB
 PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.usb.product_string=Realme XT
+    vendor.usb.product_string=Realme XT \
     vendor.usb.diag.func.name=diag \
-    vendor.usb.use_ffs_mtp=0
+    vendor.usb.dpl.inst.name=dpl \
+    vendor.usb.qdss.inst.name=qdss \
+    vendor.usb.rmnet.func.name=gsi \
+    vendor.usb.rmnet.inst.name=rmnet \
+    vendor.usb.rndis.func.name=gsi \
+    vendor.usb.use_ffs_mtp=0 \
+    vendor.usb.use_gadget_hal=0
 
 # Zygote
 PRODUCT_PROPERTY_OVERRIDES += \
