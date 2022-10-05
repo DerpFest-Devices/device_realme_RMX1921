7cd27b986fbeece90a9fb469586b53da1c0c41d0
luk1337 <priv.luk@gmail.com>
2022-10-05 17:49:01 +0000

    RMX1921: Make device name/product prop overrides work
    Change-Id: I64d11103ddb8f9dce70b9bc110745d4aafd667dc
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- derp_RMX1921.mk
+++ derp_RMX1921.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2021-2022 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -23,11 +23,15 @@ PRODUCT_MANUFACTURER := Realme
 PRODUCT_NAME := derp_RMX1921
 PRODUCT_MODEL := Realme XT
 
+PRODUCT_SYSTEM_NAME := RMX1921
+PRODUCT_SYSTEM_DEVICE := RMX1921
+
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="RMX1921-user 11 RKQ1.201217.002 1651205774457 release-keys" \
-    PRODUCT_NAME="RMX1921"
+    TARGET_DEVICE=RMX1921 \
+    TARGET_PRODUCT=RMX1921
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := realme/RMX1921/RMX1921:11/RKQ1.201217.002/1651205774457:user/release-keys
