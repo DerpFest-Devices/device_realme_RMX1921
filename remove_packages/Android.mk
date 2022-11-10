a21e7d70539e9d6dd10e5e2b9cacef4100d8f09f
kaderbava <ksbava7325@gmail.com>
2022-11-10 20:50:48 +0000

    RMX1921: Update remove packages list again
    * Cleanup according to latest derp gms sources.
    
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService CarrierSetup ConnMO DCMO DMService MyVerizonServices RecorderPrebuilt Maps YouTube Photos Drive PrebuiltGmail arcore SoundAmplifierPrebuilt SafetyHubPrebuilt SecurityHubPrebuilt Showcase USCCDM VZWAPNLib VzwOmaTrigger MusicFX GCS GoogleCamera DevicePolicyPrebuilt Chrome Chrome-Stub SCONE Tycho PixelLiveWallpaperPrebuilt GoogleTTS CarrierMetrics DiagnosticsToolPrebuilt NgaResources ScribePrebuilt
+LOCAL_OVERRIDES_PACKAGES := Maps YouTube Photos Drive PrebuiltGmail MusicFX
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
