5ac868a1dec10f88bf668a26d11caca8b90cc8ae
Dakkshesh <dakkshesh5@gmail.com>
2023-07-12 17:13:43 +0530

    RMX1921: Kang some dexpreopt flags from go target makefile
    * Remove SystemUI from dexpreopt. It was added in source already.
    
    Taken form [1]. Also nuke because PRODUCT_DEXPREOPT_SPEED_APPS its already handled by the rom's vendor and is not required in this case.
    
    [1]: https://android.googlesource.com/platform/build.git/+/refs/tags/android-13.0.0_r66/target/product/go_defaults_common.mk
    
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -153,7 +153,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@4.0.vendor
 
 # Dexpreopt
-PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := true
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SettingsGoogle \
     SystemUIGoogle
