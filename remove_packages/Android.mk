6db19d1e2cf31189100c7dec15b229b621f8860b
kaderbava <ksbava7325@gmail.com>
2023-11-24 14:08:02 +0000

    RMX1921: Remove more gapps
    * Users can install these apps from play store.
    
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := SoundAmplifierPrebuilt CalendarGooglePrebuilt FilesPrebuilt Photos ScribePrebuilt
+LOCAL_OVERRIDES_PACKAGES := SoundAmplifierPrebuilt CalendarGooglePrebuilt FilesPrebuilt Photos ScribePrebuilt DevicePolicyPrebuilt PrebuiltGmail Maps RecorderPrebuilt GoogleCamera YouTube
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
