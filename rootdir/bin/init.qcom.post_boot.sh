3210716b9201803cc9f0834a0600c1bd6ac7157e
Ivan Vecera <ivan@cera.cz>
2024-01-18 15:41:56 +0100

    RMX1921: rootdir: Simplify configure_read_ahead_kb_values
    All our targets have 4GB or more RAM.
    
    Change-Id: I82ecc837e06cfa8cf670921cd0985933dac47762
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -685,26 +685,13 @@ function oppo_configure_zram_parameters() {
 #endif /*VENDOR_EDIT*/
 
 function configure_read_ahead_kb_values() {
-    MemTotalStr=`cat /proc/meminfo | grep MemTotal`
-    MemTotal=${MemTotalStr:16:8}
+    echo 512 > /sys/block/mmcblk0/bdi/read_ahead_kb
+    echo 512 > /sys/block/mmcblk0rpmb/bdi/read_ahead_kb
 
     dmpts=$(ls /sys/block/*/queue/read_ahead_kb | grep -e dm -e mmc)
-
-    # Set 128 for <= 3GB &
-    # set 512 for >= 4GB targets.
-    if [ $MemTotal -le 3145728 ]; then
-        echo 128 > /sys/block/mmcblk0/bdi/read_ahead_kb
-        echo 128 > /sys/block/mmcblk0rpmb/bdi/read_ahead_kb
-        for dm in $dmpts; do
-            echo 128 > $dm
-        done
-    else
-        echo 512 > /sys/block/mmcblk0/bdi/read_ahead_kb
-        echo 512 > /sys/block/mmcblk0rpmb/bdi/read_ahead_kb
-        for dm in $dmpts; do
-            echo 512 > $dm
-        done
-    fi
+    for dm in $dmpts; do
+        echo 512 > $dm
+    done
 }
 
 function disable_core_ctl() {
