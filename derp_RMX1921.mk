e1d195f852fe8c3c2755aaa24dd6d383bbdb9a98
kaderbava <ksbava7325@gmail.com>
2024-01-22 15:27:31 +0000

    RMX1921: Remove unused flags in makefile
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- derp_RMX1921.mk
+++ derp_RMX1921.mk
@@ -6,9 +6,7 @@
 
 # Inherit some common DerpFest stuff
 TARGET_BOOT_ANIMATION_RES := 1080
-EXTRA_UDFPS_ANIMATIONS := true
 TARGET_NOT_USES_BLUR := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
 $(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Official-ify
