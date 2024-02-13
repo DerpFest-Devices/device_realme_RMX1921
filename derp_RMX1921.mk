69f71f719e96bebd642a5f4c14327b33f09d3e5e
kaderbava <ksbava7325@gmail.com>
2024-02-13 22:29:43 +0000

    RMX1921: Switch to mini gapps
    * Also adapt to new gms.
    
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- derp_RMX1921.mk
+++ derp_RMX1921.mk
@@ -7,6 +7,8 @@
 # Inherit some common DerpFest stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_NOT_USES_BLUR := true
+TARGET_USES_MINI_GAPPS := true
+TARGET_SUPPORTS_QUICK_TAP := true
 $(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Official-ify
--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := SoundAmplifierPrebuilt CalendarGooglePrebuilt FilesPrebuilt Photos ScribePrebuilt DevicePolicyPrebuilt PrebuiltGmail Maps RecorderPrebuilt GoogleCamera YouTube
+LOCAL_OVERRIDES_PACKAGES := GoogleCamera AdaptiveVPNPrebuilt CalendarGooglePrebuilt FilesPrebuilt PlayAutoInstallConfig SafetyHubPrebuilt talkback Updater
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
