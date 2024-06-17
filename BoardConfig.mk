092dba6448b21a9ba24afba6827948342ac4b287
Michael Bestas <mkbestas@lineageos.org>
2024-06-17 10:41:47 +0300

    RMX1921: Inherit from legacy QTI FCM
    Change-Id: I0bde1c273d4abe689f61e7d4ebcf80965d785489
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,6 +90,7 @@ TARGET_USES_COLOR_METADATA := true
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     vendor/derp/config/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
