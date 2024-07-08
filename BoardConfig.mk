93e2e24baff4085aa7c0f56c3ed32e71b7b4acc4
mikeNG <mikeNG@users.noreply.github.com>
2024-07-09 03:31:29 +0530

    RMX1921: Add a device specific FCM
    Change-Id: I9a36e9b33bf8c73cc0a98d441b141c3c698bc955
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,6 +89,7 @@ TARGET_USES_COLOR_METADATA := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     vendor/derp/config/device_framework_matrix.xml
