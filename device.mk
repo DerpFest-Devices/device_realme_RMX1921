52f9757cccdead44fdf27984a9476c0e97f51448
me-cafebabe <me-cafebabe@users.noreply.github.com>
2024-11-05 18:30:26 +0000

    RMX1921: Switch to AIDL USB Gadget HAL
    Change-Id: Ic561f62588232449c3bc1e887fc68defea634ecd
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -507,7 +507,7 @@ PRODUCT_PACKAGES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb \
-    android.hardware.usb.gadget@1.2-service-qti
+    android.hardware.usb.gadget-service.qti
 
 # Vibrator
 PRODUCT_PACKAGES += \
