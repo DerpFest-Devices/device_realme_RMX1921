6763623fa5b9e0a032d84fd061c193e33ee7cb15
pjgowtham <pjgowtham@gmail.com>
2024-11-13 22:32:49 +0100

    RMX1921: fingerprint: Set dim layer zOrder
    Change-Id: I881db78186fa9c7b350e79ac4ef90f53ee4cfbcf
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- fingerprint/UdfpsExtension.cpp
+++ fingerprint/UdfpsExtension.cpp
@@ -17,6 +17,10 @@
 #include <compositionengine/UdfpsExtension.h>
 #include <drm/sde_drm.h>
 
+uint32_t getUdfpsDimZOrder(uint32_t z) {
+    return z;
+}
+
 uint32_t getUdfpsZOrder(uint32_t z, bool touched) {
     return touched ? z | FOD_PRESSED_LAYER_ZORDER : z;
 }
